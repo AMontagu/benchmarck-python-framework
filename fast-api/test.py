a770f8dbe66123b8ac9e092598432b19486c53c2
Magloire NDABAGERA <magloire.ndabagera@gmail.com>
2024-11-01 15:21:49 +0100

    Magloire (#1)
    * adding data folder, app.py => code with functions, poetry files
    
    * adding test and docker(WIP)
    
    * adding docker file need to finish it
    
    * finishing dockerfile
    
    * docker file working
    
    * updating to fetch one row, using path and packag-mode
    
    * push repo
    
    ---------
    
    Co-authored-by: magloire ndabagera <magloirendabagera@gmail.com>

---
+++ fast-api/test.py
@@ -0,0 +1,43 @@
+import requests
+from time import perf_counter
+
+
+def test_simple_api(n):
+  start_time = perf_counter()
+  for x in range(n):
+    r = requests.get('http://127.0.0.1:5000/simple-api')
+  print(f'----- get char {perf_counter() - start_time} seconds ----')
+
+def test_stock_data(n):
+  start_time = perf_counter()
+  payload = {'pub_date': '2024-11-01 10:00:00', \
+    'headline': 'test', 'content': f'{n}'}
+  for x in range(n):
+    r = requests.post('http://127.0.0.1:5000/stock_data', params=payload)
+  print(f'----- stock data {perf_counter() - start_time} seconds ----')
+
+def test_get_data(n):
+  start_time = perf_counter()
+  for x in range(n):
+    r = requests.get('http://127.0.0.1:5000/fetch_data')
+  print(f'----- get data {perf_counter() - start_time} seconds ----')
+
+
+def test_return_2mb(n):
+  start_time = perf_counter()
+  for x in range(n):
+    r = requests.get('http://127.0.0.1:5000/return_2mb')
+  print(f'----- test little file{perf_counter() - start_time} seconds ----')
+
+def test_return_10mb(n):
+  start_time = perf_counter()
+  for x in range(n):
+    r = requests.get('http://127.0.0.1:5000/return_10mb')
+  print(f'-----test BIG fille{perf_counter() - start_time} seconds ----')
+
+
+test_simple_api(1000)
+test_stock_data(1000)
+test_get_data(1000)
+test_return_2mb(1000)
+test_return_10mb(1000)
